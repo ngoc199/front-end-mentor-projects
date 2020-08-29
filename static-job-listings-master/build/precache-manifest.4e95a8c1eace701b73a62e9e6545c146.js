8364785ec4b591af6e3e565c36e0812101403fe0
Minh Ngoc <minhngocnguyenduy@gmail.com>
2020-08-29 13:38:29 +0700

    Completely build the project

---
+++ static-job-listings-master/build/precache-manifest.4e95a8c1eace701b73a62e9e6545c146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777a34e12411503088442def9ade724f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4fbeb912bd1da2cd15c",
+    "url": "/static/css/main.56ee480a.chunk.css"
+  },
+  {
+    "revision": "ceababc00c8896840cd3",
+    "url": "/static/js/2.e26c83b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e26c83b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fbeb912bd1da2cd15c",
+    "url": "/static/js/main.e33fd649.chunk.js"
+  },
+  {
+    "revision": "ec8b1d6fa3abe4b9c27c",
+    "url": "/static/js/runtime-main.91fc3353.js"
+  }
+]);
